38ce55c059402ce6e960113f19c2612df8560650
Scott Kang <kyung.ho.kang@vanderbilt.edu>
2022-09-20 20:22:16 -0500

    website progress

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import resume from '../../assets/resume.pdf'
+
+const CTA = () => {
+  return (
+    <div className = 'cta'>
+        <a href = {resume} download className = 'btn'> Download Resume </a>
+        <a href = "#contact" clasName = 'btn btn-primary'> Let's Talk </a>
+    </div>
+  )
+}
+
+export default CTA
