ef6debd0c3b1c873a6273a28d5d133efe229c18e
scottkang123 <48114611+scottkang123@users.noreply.github.com>
2024-01-04 23:43:39 +0900

    added network analysis paper

---
+++ src/components/portfolio/NetworkAnalysis.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import paper from '../../assets/DDI_Network_paper_Scott.pdf'
+
+const ML = () => {
+  return (
+    <div className = 'puc'>
+        <a href = {paper} download className = 'btn'> Drug Drug Interaction Network Analysis </a>
+    </div>
+  )
+}
+
+export default ML
